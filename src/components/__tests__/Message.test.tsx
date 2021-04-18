06b1d3c937e1a1115f2a91855a39d9b4d236e878
Zeztron <pateltharsh@gmail.com>
2021-04-18 11:59:19 -0400

    test for prompting the user to start the game

--- src/components/__tests__/Message.test.tsx
+++ src/components/__tests__/Message.test.tsx
@@ -3,15 +3,19 @@ import { shallow, ShallowWrapper } from 'enzyme';
 import Message from '../Message';
 import { findByTestAttr } from '../../test/testUtils';
 
-const setup = () => shallow(<Message />);
+const setup = (props = { hasStarted: false, isXNext: true }) =>
+  shallow(<Message {...props} />);
 
 describe('Message Component', () => {
-  let wrapper: ShallowWrapper;
-
-  beforeEach(() => (wrapper = setup()));
-
   it('renders outer div component without errors', () => {
+    const wrapper: ShallowWrapper = setup();
     const component = findByTestAttr(wrapper, 'message-component');
     expect(component.length).toBe(1);
+  });
+
+  it('prompts the user at the start of the game', () => {
+    const wrapper: ShallowWrapper = setup();
+    const messageComponent = findByTestAttr(wrapper, 'message-component');
+    expect(messageComponent.text()).toBe('Click To Start Game');
   });
 });
