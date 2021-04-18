6964b62d212d66e4afa8bf2836daaf9121571d74
Zeztron <pateltharsh@gmail.com>
2021-04-18 12:50:41 -0400

    test to display X and O in their respective squares

--- src/components/__tests__/Game.test.tsx
+++ src/components/__tests__/Game.test.tsx
@@ -22,4 +22,13 @@ describe('Game Component', () => {
   it('renders an instance of Message component successfully', () => {
     expect(wrapper.find('Message').length).toEqual(1);
   });
+
+  it('Displays X and O in their respective squares', () => {
+    const x = findByTestAttr(wrapper, 'square-button-0');
+    const o = findByTestAttr(wrapper, 'square-button-1');
+    x.simulate('click');
+    o.simulate('click');
+    expect(x.text()).toBe('X');
+    expect(o.text()).toBe('O');
+  });
 });
