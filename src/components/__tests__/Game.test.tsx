144708a5d9b0f41443a04bbbbcece01c04c944de
Zeztron <pateltharsh@gmail.com>
2021-04-18 09:02:01 -0400

    test if board component renders successfully

--- src/components/__tests__/Game.test.tsx
+++ src/components/__tests__/Game.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { mount, ReactWrapper } from 'enzyme';
 import Game from '../Game';
 import { findByTestAttr } from '../../test/testUtils';
+import Board from '../Board';
 
 const setup = () => mount(<Game />);
+
+const boardProps = {
+  squares: Array(9).fill(null),
+  onClick: jest.fn(),
+};
 
 describe('Game Component', () => {
   let wrapper: ReactWrapper;
@@ -12,5 +18,11 @@ describe('Game Component', () => {
   it('renders without crashing', () => {
     const gameComponent = findByTestAttr(wrapper, 'game-component');
     expect(gameComponent.length).toBe(1);
+  });
+
+  it('renders Board component successfully', () => {
+    expect(wrapper.containsMatchingElement(<Board {...boardProps} />)).toBe(
+      true
+    );
   });
 });
