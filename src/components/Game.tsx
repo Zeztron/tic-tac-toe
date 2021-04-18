feb3b6316ba2d845efd8c31f2de5b7e5fc741739
Zeztron <pateltharsh@gmail.com>
2021-04-18 12:19:34 -0400

    render message component successfully

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import Board from './Board';
+import Message from './Message';
 
 const Game = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
 
   const handleClick = () => {};
 
   return (
     <div data-test='game-component'>
       <Board squares={squares} onClick={handleClick} />
+      <Message
+        hasStarted={squares.some((square) => square)}
+        isXNext={isXNext}
+      />
     </div>
   );
 };
