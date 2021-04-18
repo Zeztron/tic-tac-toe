78aac113dfd4b12b92d74edb602c626e878b383d
Zeztron <pateltharsh@gmail.com>
2021-04-18 15:58:42 -0400

    test if rewind component button can handle click

--- src/components/__tests__/Rewind.test.tsx
+++ src/components/__tests__/Rewind.test.tsx
@@ -4,11 +4,19 @@ import Rewind from '../Rewind';
 import { findByTestAttr } from '../../test/testUtils';
 
 describe('Rewind Component', () => {
+  const rewind = jest.fn();
   let wrapper: ShallowWrapper;
   beforeEach(() => (wrapper = shallow(<Rewind />)));
 
   it('renders Rewind component without errors', () => {
     const component = findByTestAttr(wrapper, 'rewind-component');
     expect(component.length).toEqual(1);
+  });
+
+  it('handles Rewind button click', () => {
+    rewind.mockClear();
+    const component = findByTestAttr(wrapper, 'rewind-component');
+    component.simulate('click');
+    expect(rewind).toBeCalled();
   });
 });
