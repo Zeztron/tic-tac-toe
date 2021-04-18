2e5a889a918532bec92ff47e85f497508eff9733
Zeztron <pateltharsh@gmail.com>
2021-04-18 15:43:11 -0400

    test to see if reset button handles click

--- src/components/__tests__/Reset.test.tsx
+++ src/components/__tests__/Reset.test.tsx
@@ -3,13 +3,20 @@ import { shallow, ShallowWrapper } from 'enzyme';
 import Reset from '../Reset';
 import { findByTestAttr } from '../../test/testUtils';
 
-const setup = () => shallow(<Reset />);
-
 describe('Reset Component', () => {
-  const wrapper: ShallowWrapper = setup();
+  const reset = jest.fn();
+  let wrapper: ShallowWrapper;
+  beforeEach(() => (wrapper = shallow(<Reset reset={reset} />)));
 
   it('renders Reset component without errors', () => {
     const component = findByTestAttr(wrapper, 'reset-component');
     expect(component.length).toEqual(1);
+  });
+
+  it('handles Reset button click', () => {
+    reset.mockClear();
+    const component = findByTestAttr(wrapper, 'reset-component');
+    component.simulate('click');
+    expect(reset).toBeCalled();
   });
 });
