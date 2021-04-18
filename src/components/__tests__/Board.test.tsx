a17de2f3e1078f0e32551cb8c4dc468c19a2d7ff
Zeztron <pateltharsh@gmail.com>
2021-04-18 08:43:49 -0400

    test to display square components in Board

--- src/components/__tests__/Board.test.tsx
+++ src/components/__tests__/Board.test.tsx
@@ -1,15 +1,26 @@
-import { shallow, ShallowWrapper } from 'enzyme';
+import { shallow, ShallowWrapper, mount } from 'enzyme';
 import { findByTestAttr } from '../../test/testUtils';
 import Board from '../Board';
+import Square from '../Square';
 
-const setup = () => shallow(<Board />);
+const setupProps = {
+  squares: Array(9).fill(null),
+  onClick: jest.fn(),
+};
+
+const setup = () => shallow(<Board {...setupProps} />);
 
 describe('Board Component', () => {
   let wrapper: ShallowWrapper;
+
   beforeEach(() => (wrapper = setup()));
 
-  it('renders component without errors', () => {
+  it('renders outer div component without errors', () => {
     const component = findByTestAttr(wrapper, 'board-component');
     expect(component.length).toBe(1);
+  });
+
+  it('displays the specified amount of Square components to represent the tic-tac-toe grid', () => {
+    expect(wrapper.find(Square).length).toEqual(setupProps.squares.length);
   });
 });
