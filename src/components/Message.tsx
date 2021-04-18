caa7f8c15472040cc006514d384b96c8add7471f
Zeztron <pateltharsh@gmail.com>
2021-04-18 12:08:43 -0400

    successfully shows that it's O's turn if xisnext is false

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -8,7 +8,11 @@ interface MessageProps {
 const Message: React.FC<MessageProps> = ({ hasStarted, isXNext }) => {
   return (
     <div data-test='message-component'>
-      {hasStarted ? (isXNext ? "It's X's Turn" : '') : 'Click To Start Game'}
+      {hasStarted
+        ? isXNext
+          ? "It's X's Turn"
+          : "It's O's Turn"
+        : 'Click To Start Game'}
     </div>
   );
 };
